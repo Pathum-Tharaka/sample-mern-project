079157155cbfb401625ac9068778a11f76ce0265
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-04-07 08:28:59 +0530

    death

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,31 @@
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import UserManage from './Pages/UserManage';
-import AllUsers from './Pages/AllUsers';
-import AddUsers from './Pages/AddUsers';
-import NoPages from './Pages/NoPages';
-import Supplier from './Pages/Supplier';
-import AllProjects from './Pages/Allprojects';
-import NewProjects from './Pages/Newprojects';
-import ProjectCategory from './Pages/Projectcategory';
-
-
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import UserManage from "./Pages/UserManage";
+import AllUsers from "./Pages/AllUsers";
+import AddUsers from "./Pages/AddUsers";
+import NoPages from "./Pages/NoPages";
+import Supplier from "./Pages/Supplier";
+import Allprojects from "./Pages/Allprojects";
+import Addprojects from "./Pages/Addprojects";
+import ProjectCategory from "./Pages/Projectcategory";
 
 function App() {
   return (
     <div>
-
       <BrowserRouter>
-      <Routes>
-
-        <Route index element = {<Home/>}/>
-        <Route path = '/dashboard' element = {<Home/>}/>
-        <Route path = '/user' element = {<UserManage/>}/>
-        <Route path = '/user/addusers' element = {<AddUsers/>}/>
-        <Route path = '/user/allusers' element = {<AllUsers/>}/>
-        <Route path = '/supplier' element = {<Supplier/>}/>
-        <Route path = '/Allprojects' element = {<AllProjects/>}/>
-        <Route path = '/Newprojects' element = {<NewProjects/>}/>
-        <Route path = '/Projectcategory' element = {<ProjectCategory/>}/>
-
-       <Route path = '*' element = {<NoPages/>}/>
-
-
-
-
-
-      </Routes>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/user" element={<UserManage />} />
+          <Route path="/user/addusers" element={<AddUsers />} />
+          <Route path="/user/allusers" element={<AllUsers />} />
+          <Route path="/supplier" element={<Supplier />} />
+          <Route path="/Allprojects" element={<Allprojects />} />
+          <Route path="/Addprojects" element={<Addprojects />} />
+          <Route path="/Projectcategory" element={<ProjectCategory />} />
+          <Route path="*" element={<NoPages />} />
+        </Routes>
       </BrowserRouter>
-
     </div>
   );
 }
